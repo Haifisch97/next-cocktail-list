9ba18a74b025982f9b1443dca87caebf974734f5
Haifisch97 <72409661+Haifisch97@users.noreply.github.com>
2023-09-06 12:53:19 +0300

    start work

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,14 +4,21 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
+  title: 'Cocktail App',
+  description: 'The best Cocktail app!',
+  image: './favicon.ico',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
